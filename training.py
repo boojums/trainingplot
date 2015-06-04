491fee8b5abd6249eb60adb1aca48e3d20bd0723
Cristina Luis <cristina.luis@met.no>
2015-06-04 16:03:37 +0200

    read jj file and do a diff

--- training.py
+++ training.py
@@ -3,6 +3,8 @@
 import shutil
 import datetime as dt
 import argparse
+import datetime as dt
+from collections import OrderedDict
 
 import mechanize
 import csv
@@ -11,8 +13,13 @@ matplotlib.use('Agg')   # necessary for remote usage to avoid DISPLAY error
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from bokeh.plotting import ColumnDataSource, figure, show, output_file, save
+from bokeh.models import HoverTool
 
 from settings import *
+
+
+months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
 # TODO: consider using Counter class from collections to count neat stats 
 # TODO: small sql db for learning/easy querying?
@@ -59,7 +66,16 @@ def main():
     df['moday'] = map(lambda month, day:str(month) + '-' + str(day), df['month'], df['dom'])
 
     df = df[df['activity']=='Orienteering']
-    plot_day_counts(df, name)
+
+    jjdata = read_jj('data/jj.txt')
+    source = get_day_counts_diff_source(jjdata, df)
+    #s1 = plot_counts_data(df, title='Cristina')
+    #s2 = plot_counts_data(jj, title='J-J')
+    plot_counts_data(source, 'Cristina-J-J')
+
+    #filename = 'cristina_jj.html'
+    #p = vplot(s1,s2,s3)
+    #save(p)
 
 def plot_xkcd(data):
     running = df[df['activity'] == 'Running']
@@ -85,16 +101,11 @@ def orienteering_days_of_year(data):
     alldates = [str(x.month) + '-' + str(x.day) for x in [dt.date(1996, 1,1)+dt.timedelta(days=i) for i in range(366)]]
     missing = [x for x in alldates if x not in modays]
 
-def plot_day_counts(data, name='test'):
-    from collections import OrderedDict
-
-    from bokeh.plotting import ColumnDataSource, figure, show, output_file, save
-    from bokeh.models import HoverTool
+def get_day_counts_source(data):
     
     colors = [
         '#ffffff', '#efedf5', '#dadaeb', '#bcbddc', '#9e9ac8', '#807dba', '#6a51a3', '#54278f', '#3f007d'
     ]
-    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
     days = range(1, 32)
     # We need to have values for every
     # pair of month/day names. Map the count to a color.
@@ -119,12 +130,71 @@ def plot_day_counts(data, name='test'):
         data=dict(month=month, day=day, color=color, count=count)
     )
 
-    filename = 'odays_{}.html'.format(name.lower())
+    return source
+
+def get_day_counts_diff_source(data1, data2):
+    
+    colors = [
+        '#af8dc3', '#f7f7f7', '#7fbf7b', '#666666'
+        ]
+    days = range(1, 32)
+    # We need to have values for every
+    # pair of month/day names. Map the count to a color.
+    month = []
+    day = []
+    color = []
+    count = []
+    counts1 = data1.moday.value_counts()
+    counts2 = data2.moday.value_counts()
+    for x, m in enumerate(months):
+        for d in days:
+            month.append(m)
+            day.append(d)
+            try:
+                daily_count1 = counts1[str(x+1)+'-'+str(d)]
+            except KeyError:
+                daily_count1 = 0
+            try:
+                daily_count2 = counts2[str(x+1)+'-'+str(d)]
+            except KeyError:
+                daily_count2 = 0
+
+            if ((d == 31) and (m in ['Feb', 'Apr', 'Jun', 'Sep', 'Nov'])) or ((m == 'Feb') and (d == 30)):
+                daily_count = 'Both'
+                c = 1
+            elif (daily_count1 == 0) and (daily_count2 > 0):
+                daily_count = 'Cristina'
+                c = 0
+            elif (daily_count2 == 0) and (daily_count1 > 0):
+                daily_count = 'J-J'
+                c = 2
+            elif (daily_count2 == 0) and (daily_count1 == 0):
+                daily_count = 'Neither'
+                c = 3
+            else:
+                daily_count = 'Both'
+                c = 1
+
+            count.append(daily_count)
+            color.append(colors[c])
+
+    source = ColumnDataSource(
+        data=dict(month=month, day=day, color=color, count=count)
+    )
+
+    return source
+
+def plot_counts_data(source, title='Test'):
+    ''' Create day/month grid plot of data. 
+    source is a ColumnDataSource with month, day, color, count
+    '''
+
+    filename = 'odays_{}.html'.format(title.lower())
     output_file(filename, mode="cdn")
 
     TOOLS = "resize,hover,save,pan,box_zoom,wheel_zoom"
 
-    p = figure(title="{}'s orienteering sessions per day of year".format(name),
+    p = figure(title=title,
         x_range=[1,31], y_range=list(reversed(months)),
         x_axis_location="above", plot_width=900, plot_height=400,
         toolbar_location="left", tools=TOOLS)
@@ -145,8 +215,23 @@ def plot_day_counts(data, name='test'):
         ('count', '@count'),
     ])
 
-    save(p)      # show the plot    
+    #return p
+    show(p)      # show the plot    
 
+
+def read_jj(filename):
+    ''' Read in list of dates in 12/19/1978 format'''
+    with open(filename) as f:
+        dates = [line.strip().split('/') for line in f]
+
+    dates = [dt.date(int(x[2]), int(x[0]), int(x[1])) for x in dates]
+    month = map(lambda date: date.month, dates)
+    dom = map(lambda date: date.day, dates)
+    moday = map(lambda month, day:str(month) + '-' + str(day), month, dom)
+
+    data = pd.DataFrame({'date': dates, 'month':month , 'dom': dom, 'moday':moday})
+
+    return data
 
 def get_all_training_data():
     ''' Use mechanize to browse AP and retrieve my training.'''
